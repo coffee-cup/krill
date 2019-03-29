daed6fa893c6a51cac931a157ae0a551962dbeef
Jake Runzer <jakerunzer@gmail.com>
2019-03-29 11:50:00 +0000

    map builtin type checking

--- src/Eval/BuiltIn.hs
+++ src/Eval/BuiltIn.hs
@@ -5,6 +5,7 @@ module Eval.BuiltIn where
 
 import           Control.Monad
 import           Control.Monad.Except
+import           Control.Monad.State
 import           Data.Text.Lazy.IO    as T
 
 import           Eval.Value
@@ -33,14 +34,30 @@ length l v = case v of
   _ -> throwError $ TypeMismatch l "argument to be list" v
 
 map :: Loc -> Value -> Eval Value
-map l1 arg1 =
-  return $ BuiltIn $ BFunc mapFunc
+map l1 arg1 = do
+  checkFnArg l1 arg1
+  return $ BuiltIn $ BFunc (\l2 arg2 -> do
+                               checkListArg l2 arg2
+                               evalApp l2 arg1 arg2)
   where
-    mapFunc :: Loc -> Value -> Eval Value
-    mapFunc l2 arg2 =
-      case arg1 of
-        Lambda (IFunc fn) _ ->
-          case arg2 of
-            List xs -> do
-              ys <- mapM fn xs
-              return $ List ys
+    checkFnArg :: Loc -> Value -> Eval ()
+    checkFnArg l arg =
+      case arg of
+        Lambda _ _ -> return ()
+        BuiltIn _  -> return ()
+        _ -> throwError $ TypeMismatch l "argument to be a function" arg
+    checkListArg :: Loc -> Value -> Eval ()
+    checkListArg l arg =
+      case arg of
+        List _ -> return ()
+        _      -> throwError $ TypeMismatch l "argument to be a list" arg
+    evalApp :: Loc -> Value -> Value -> Eval Value
+    evalApp _ (Lambda (IFunc fn) env) (List xs) = do
+      oldEnv <- gets _env
+      modify (\st -> st { _env = env })
+      ys <- mapM fn xs
+      modify (\st -> st { _env = oldEnv })
+      return $ List ys
+    evalApp l (BuiltIn (BFunc fn)) (List xs) = do
+      ys <- mapM (fn l) xs
+      return $ List ys
